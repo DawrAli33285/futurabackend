ef92428db068ae9a861bc0b9c74185730d289258
DawrAli33285 <lemightyeagle@gmail.com>
2026-07-21 02:33:36 -0700

    new

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 
-const uploadDir = path.join(__dirname, '..', 'uploads', 'items');
+const uploadDir = '/tmp/public/files/pdf'
 
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
